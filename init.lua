cd2b16a8be85ae3c12363304045a709b55f3cca9
Graffioh <bregliascuola2002@gmail.com>
2024-07-23 12:20:41 +0200

    Modify init.lua

--- init.lua
+++ init.lua
@@ -284,7 +284,9 @@ require('lazy').setup({
     main = 'ibl',
     opts = {},
     config = function()
-      require('ibl').setup()
+      require('ibl').setup {
+        scope = { enabled = false },
+      }
     end,
   },
 
